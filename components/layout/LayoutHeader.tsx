9f74926167747f535134971da8663fb767c1856b
kunul942 <jhonatanlee942@gmail.com>
2024-02-27 22:31:57 -0600

    done

--- components/layout/LayoutHeader.tsx
+++ components/layout/LayoutHeader.tsx
@@ -1,3 +1,4 @@
+import { homeMainImage } from '@/assets';
 import Head from 'next/head';
 
 interface Props {
@@ -17,8 +18,8 @@ export const LayoutHeader = ({ title }: Props) => {
             <meta name='author' content='Wedding Jose Daniel' />
             <meta name='keywords' content='Wedding Jose Daniel' />
             <link rel='canonical' href='https://www.weddingjosedaniel.com/' />
-            {/* <link rel='icon' href={kunuLogo.src} /> */}
-            {/* <link rel='apple-touch-icon' href={kunuLogo.src} /> */}
+            <link rel='icon' href={homeMainImage.src} />
+            <link rel='apple-touch-icon' href={homeMainImage.src} />
             <meta property='og:type' content='website' />
             <meta
                 property='og:url'
@@ -38,6 +39,7 @@ export const LayoutHeader = ({ title }: Props) => {
                 name='twitter:description'
                 content='¡Hola! Soy José Daniel. Si me permites seré tu fotográfo. Comencé en este mundo de las bodas cuando me casé. Compré mi camara para que alguien me hiciera fotos con ella, me regalaron un taller al comprarla y me di cuenta de que no era nada fácil, pero también de que hacer fotos era mi pasión. Así que decidí dedicar horas para aprender, hasta que finalmente decidí ser feliz haciendo lo que amo y gracias a personas como tu que han confiado en mi trabajo, hoy estoy aquí documentando el día más feliz de los enamorados.'
             />
+            <meta name='twitter:image' content={homeMainImage.src} />
         </Head>
     );
 };
