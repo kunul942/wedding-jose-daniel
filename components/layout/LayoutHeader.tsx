bc5a8135180b313a9f55bb7c995bebd26adb80cc
kunul942 <jhonatanlee942@gmail.com>
2024-03-01 15:38:13 -0600

    fixing logo

--- components/layout/LayoutHeader.tsx
+++ components/layout/LayoutHeader.tsx
@@ -31,7 +31,7 @@ export const LayoutHeader = ({ title }: Props) => {
                 property='og:description'
                 content={`¡Hola! Soy José Daniel. Si me permites seré tu fotográfo. Comencé en este mundo de las bodas cuando me casé. Compré mi camara para que alguien me hiciera fotos con ella, me regalaron un taller al comprarla y me di cuenta de que no era nada fácil, pero también de que hacer fotos era mi pasión. Así que decidí dedicar horas para aprender, hasta que finalmente decidí ser feliz haciendo lo que amo y gracias a personas como tu que han confiado en mi trabajo, hoy estoy aquí documentando el día más feliz de los enamorados.`}
             />
-            <meta property='og:image' />
+            <meta property='og:image' content={homeMainImage.src} />
             <meta name='twitter:card' content='summary_large_image' />
             <meta name='twitter:site' content='@weddingjosedaniel' />
             <meta name='twitter:creator' content='@weddingjosedaniel' />
